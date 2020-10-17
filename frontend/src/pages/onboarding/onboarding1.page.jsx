25d1524c4ea577d594fb4ee873a653a9e2ede45f
Darren <darrenzhang200027@gmail.com>
2020-10-17 16:40:22 -0400

    added onboarding1 - link bank page

--- frontend/src/pages/onboarding/onboarding1.page.jsx
+++ frontend/src/pages/onboarding/onboarding1.page.jsx
@@ -3,37 +3,60 @@ import BA from './images/ba.png'
 import Citi from './images/citi.png'
 import TD from './images/td.png'
 import WF from './images/wf.jpeg'
+import ICBC from './images/icbc.png'
+import SIB from './images/sib.png'
+import WHB from './images/whb.png'
+import FCB from './images/fcb.png'
 import "./onboarding1.css"
+import { Button } from "@material-ui/core"
 
 export default function Onboarding1(props) {
     return (
-        <div>
+        <div className="banks">
             <h1>Link Bank?</h1>
-            <div className="banks">
 
-                <div>
-                    <img src={BA}/>
-                    <button>Bank of America</button>
-                </div>
+            <div>
+                <img src={BA} />
+                <Button fullWidth={true} variant="contained" color="primary" >Bank of America</Button>
+            </div>
 
-                <div>
-                    <img src={WF}/>
-                    <button>Wells Fargo</button>
-                </div>
+            <div>
+                <img src={WF} />
+                <Button fullWidth={true} variant="contained" color="primary">Wells Fargo</Button>
+            </div>
 
-                <div>
-                    <img src={Citi}/>
-                    <button>Citibank</button>
-                </div>
+            <div>
+                <img src={Citi} />
+                <Button fullWidth={true} variant="contained" color="primary"> Citibank</Button>
+            </div>
 
-                <div>
-                    <img src={TD}/>
-                    <button>TD Bank</button>
-                </div>
+            <div>
+                <img src={TD} />
+                <Button fullWidth={true} variant="contained" color="primary">TD Bank</Button>
+            </div>
 
-                <div>
-                    <button>Other</button>
-                </div>
+            <div>
+                <img src={ICBC} />
+                <Button fullWidth={true} variant="contained" color="primary">ICBC</Button>
+            </div>
+
+            <div>
+                <img src={FCB} />
+                <Button fullWidth={true} variant="contained" color="primary">First Citizen Bank</Button>
+            </div>
+
+            <div>
+                <img src={WHB} />
+                <Button fullWidth={true} variant="contained" color="primary">World Houston Bank</Button>
+            </div>
+
+            <div>
+                <img src={SIB} />
+                <Button fullWidth={true} variant="contained" color="primary">Stanbic ITBC Bank</Button>
+            </div>
+
+            <div>
+                <Button fullWidth={true} variant="contained" color="primary">Other</Button>
             </div>
         </div>
     )
