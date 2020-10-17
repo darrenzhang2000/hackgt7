1c3fd27ec17001df9af26a259f32a4e372b1bb97
Darren <darrenzhang200027@gmail.com>
2020-10-17 18:31:30 -0400

    added onboarding2 - decide checkings or savings account

--- frontend/src/pages/onboarding/onboarding2.page.jsx
+++ frontend/src/pages/onboarding/onboarding2.page.jsx
@@ -1,9 +1,16 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
+import "./onboarding2.css"
 
 export default function onboarding2(props) {
     return (
-        <div>
-            hi
+        <div className="root">
+            <h1>Which account?</h1>
+            <Button className="button" variant="contained" color="primary">Checking</Button>
+            <div className="space"></div>
+            <Button className="button" variant="contained" color="primary">Savings</Button>
+            <div className="space"></div>
+            <Button className="button" variant="contained" color="default">Back</Button>
         </div>
     )
 }
